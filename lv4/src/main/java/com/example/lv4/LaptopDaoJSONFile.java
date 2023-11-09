82667ad0eafe12e04d0c4bd7e43b048cb7a183f4
AnesCenanovic <127337745+AnesCenanovic@users.noreply.github.com>
2023-11-09 15:18:06 +0100

    Dodane ostale klase

--- lv4/src/main/java/com/example/lv4/LaptopDaoJSONFile.java
+++ lv4/src/main/java/com/example/lv4/LaptopDaoJSONFile.java
@@ -2,8 +2,61 @@ package com.example.lv4;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 public class LaptopDaoJSONFile implements LaptopDao {
+
     private File file;
     private ArrayList<Laptop> laptopi;
+
+    public LaptopDaoJSONFile(File file){
+        this.file=file;
+        this.laptopi = new ArrayList<>();
+    }
+
+    @Override
+    public void dodajLaptopUListu(Laptop laptop){
+        this.laptopi = vratiPodatkeIzDatoteke(); // prvo vrati postojecu listu
+        laptopi.add(laptop);
+    }
+    @Override
+    public Laptop getLaptop(String procesor){
+        this.laptopi = vratiPodatkeIzDatoteke();
+        for(Laptop laptop: laptopi){
+            if(laptop.getProcesor().equals(procesor)){
+                return laptop;
+            }
+        }
+        return null;
+    }
+    @Override
+    public ArrayList<Laptop> vratiPodatkeIzDatoteke(){
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayList<Laptop> result = new ArrayList<>();
+        try { result = mapper.readValue(file, new TypeReference<ArrayList<Laptop>>() {}); {
+        }
+        }
+        catch (IOException except){
+            except.printStackTrace();
+        }
+        return result;
+    }
+    @Override
+    public void dodajLaptopUFile(Laptop laptop){
+        this.laptopi = vratiPodatkeIzDatoteke(); // prvo vrati postojecu listu
+        laptopi.add(laptop);
+        // zapisi u datoteku
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayList<Laptop> result = new ArrayList<>();
+        try { mapper.writeValue(file,result.getClass());
+            this.laptopi=result;
+        }
+        catch (IOException except){
+            except.printStackTrace();
+        }
+    }
 }
--- lv4/src/main/java/com/example/lv4/LaptopDaoXMLFile.java
+++ lv4/src/main/java/com/example/lv4/LaptopDaoXMLFile.java
@@ -1,10 +1,58 @@
 package com.example.lv4;
-import java.io.File;
+
+import java.io.*;
 import java.util.*;
+import java.io.IOException;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 public class LaptopDaoXMLFile implements LaptopDao{
     private File file;
     private ArrayList<Laptop> laptopi;
 
+    public LaptopDaoXMLFile(File file){
+        this.file=file;
+        this.laptopi = new ArrayList<>();
+    }
 
+    @Override
+    public void dodajLaptopUListu(Laptop laptop){
+        this.laptopi = vratiPodatkeIzDatoteke(); // prvo vrati postojecu listu
+        laptopi.add(laptop);
+    }
+    @Override
+    public Laptop getLaptop(String procesor){
+        this.laptopi = vratiPodatkeIzDatoteke();
+        for(Laptop laptop: laptopi){
+            if(laptop.getProcesor().equals(procesor)){
+                return laptop;
+            }
+        }
+        return null;
+    }
+    @Override
+    public ArrayList<Laptop> vratiPodatkeIzDatoteke(){
+        XmlMapper mapper = new XmlMapper();
+        ArrayList<Laptop> result = new ArrayList<>();
+        try { result = mapper.readValue(file,result.getClass());
+        }
+        catch (IOException except){
+            except.printStackTrace();
+        }
+        return result;
+    }
+    @Override
+    public void dodajLaptopUFile(Laptop laptop){
+        this.laptopi = vratiPodatkeIzDatoteke(); // prvo vrati postojecu listu
+        laptopi.add(laptop);
+        // zapisi u datoteku
+        XmlMapper mapper = new XmlMapper();
+        ArrayList<Laptop> result = new ArrayList<>();
+        try { mapper.writeValue(file,result.getClass());
+            this.laptopi=result;
+        }
+        catch (IOException except){
+            except.printStackTrace();
+        }
+    }
 }
